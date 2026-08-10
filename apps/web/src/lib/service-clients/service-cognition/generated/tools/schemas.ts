8993fc29194ad6313f77fc1254fc5bd56e239f29
Eric Hayes <56524199+ehayes2000@users.noreply.github.com>
2026-08-10 17:53:26 -0400

    feat: wasm fold (#5503)

--- apps/web/src/lib/service-clients/service-cognition/generated/tools/schemas.ts
+++ apps/web/src/lib/service-clients/service-cognition/generated/tools/schemas.ts
@@ -763,8 +763,8 @@ export const CreateImportEntityResponse = z.object({
 });
 
 export const CreateProject = z.object({
-  parentProjectId: z.union([z.string().uuid(), z.null()]).optional(),
   projectName: z.string(),
+  parentProjectId: z.union([z.string().uuid(), z.null()]).optional(),
 });
 
 export const CreateProjectResponse = z.object({
@@ -1702,6 +1702,7 @@ export const ListNotifications = z.object({
           'call',
           'task',
           'github',
+          'reminder',
         ])
       ),
       z.null(),
@@ -1850,7 +1851,6 @@ export const MarkNotificationsSeen = z.object({
 });
 
 export const MoveToProject = z.object({
-  entityId: z.string().uuid(),
   entityType: z.any().superRefine((x, ctx) => {
     const schemas = [
       z.literal('document'),
@@ -1874,12 +1874,13 @@ export const MoveToProject = z.object({
       });
     }
   }),
+  entityId: z.string().uuid(),
   projectId: z.union([z.string().uuid(), z.null()]).optional(),
 });
 
 export const MoveToProjectResponse = z.object({
-  message: z.string(),
   success: z.boolean(),
+  message: z.string(),
 });
 
 export const NameSearch = z.object({
@@ -3038,9 +3039,11 @@ export const ReadMetadataResponse = z.object({
 export const ReadProject = z.object({ projectId: z.string().uuid() });
 
 export const ReadProjectResponse = z.object({
+  projectId: z.string().uuid(),
+  projectName: z.string(),
+  parentProjectId: z.union([z.string(), z.null()]).optional(),
   items: z.array(
     z.object({
-      fileType: z.union([z.string(), z.null()]).optional(),
       id: z.string(),
       itemType: z.any().superRefine((x, ctx) => {
         const schemas = [
@@ -3065,14 +3068,12 @@ export const ReadProjectResponse = z.object({
         }
       }),
       name: z.string(),
+      fileType: z.union([z.string(), z.null()]).optional(),
       updatedAt: z
         .union([z.string().datetime({ offset: true }), z.null()])
         .optional(),
     })
   ),
-  parentProjectId: z.union([z.string(), z.null()]).optional(),
-  projectId: z.string().uuid(),
-  projectName: z.string(),
 });
 
 export const RenameDocument = z.object({
@@ -3087,6 +3088,7 @@ export const RenameDocumentResponse = z.object({
 });
 
 export const SearchSkills = z.object({
+  name: z.string(),
   matchType: z
     .any()
     .superRefine((x, ctx) => {
@@ -3108,7 +3110,6 @@ export const SearchSkills = z.object({
       }
     })
     .optional(),
-  name: z.string(),
 });
 
 export const SearchSkillsResponse = z.object({
